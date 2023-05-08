e884c2337dd42e579e03962199c8721aa7455340
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2023-05-08 23:28:59 +0300

    Expence

--- src/main/java/com/example/springbootmongodb/Model/Expence.java
+++ src/main/java/com/example/springbootmongodb/Model/Expence.java
@@ -5,6 +5,14 @@ import java.math.BigDecimal;
 public class Expence {
     private String id;
     private String expenceName;
+
+    public Expence(String id, String expenceName, ExpenseCategory expenseCategory, BigDecimal expenceAmount) {
+        this.id = id;
+        this.expenceName = expenceName;
+        this.expenseCategory = expenseCategory;
+        this.expenceAmount = expenceAmount;
+    }
+
     private ExpenseCategory expenseCategory;
     private BigDecimal expenceAmount;
 }
