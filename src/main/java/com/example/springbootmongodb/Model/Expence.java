32975af443cba277c6f67b23c7c88c7111bc9473
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2023-05-08 23:26:08 +0300

    Expence

---
+++ src/main/java/com/example/springbootmongodb/Model/Expence.java
@@ -0,0 +1,10 @@
+package com.example.springbootmongodb.Model;
+
+import java.math.BigDecimal;
+
+public class Expence {
+    private String id;
+    private String expenceName;
+    private ExpenceCategory expenceCategory;
+    private BigDecimal expenceAmount;
+}
