ee91aba9c41e4e9d01382a44d1177dc2e2f0c246
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2023-05-11 20:51:24 +0300

    add methods findByName

--- src/main/java/com/example/springbootmongodb/repository/ExpenseRepository.java
+++ src/main/java/com/example/springbootmongodb/repository/ExpenseRepository.java
@@ -2,6 +2,11 @@ package com.example.springbootmongodb.repository;
 
 import com.example.springbootmongodb.Model.Expense;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
 
 public interface ExpenseRepository extends MongoRepository<Expense, String> {
+    @Query("{'name': ?0")
+    Optional<Expense>findByName(String name);
 }
