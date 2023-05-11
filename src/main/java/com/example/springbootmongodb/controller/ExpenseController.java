290c933543efee1d5ca06126c8556ac146509684
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2023-05-11 20:51:06 +0300

    add methods

--- src/main/java/com/example/springbootmongodb/controller/ExpenseController.java
+++ src/main/java/com/example/springbootmongodb/controller/ExpenseController.java
@@ -1,14 +1,47 @@
 package com.example.springbootmongodb.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springbootmongodb.Model.Expense;
+import com.example.springbootmongodb.service.ExpenseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/expense")
 public class ExpenseController {
-    public void addExpense() {}
-    public void updateExpense() {}
-    public void getAllExpense() {}
-    public void getExpenseByName() {}
-    public void deleteExpense() {}
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @PostMapping
+    public ResponseEntity addExpense(@RequestBody Expense expense) {
+        expenseService.addExpense(expense);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping
+    public ResponseEntity updateExpense(@RequestBody Expense expense) {
+        expenseService.updateExpense(expense);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Expense>> getAllExpense() {
+       return ResponseEntity.ok(expenseService.getAllExpense());
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Expense> getExpenseByName(@PathVariable String name) {
+        return ResponseEntity.ok(expenseService.getExpenseByName(name));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteExpense(String id) {
+        expenseService.deleteExpense(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
